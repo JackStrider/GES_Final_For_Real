37ad45932e89e7eb35ac6b6fff5c7070671ece28
JackStrider <rossi.william@gmail.com>
2017-12-10 21:42:51 -0600

    First commit

---
+++ Assets/Scripts/GenericInteractiveObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericInteractiveObject : MonoBehaviour, IActivatable
+{
+    [SerializeField]
+    private string nameText;
+    public string NameText
+    {
+        get
+        {
+            return nameText;
+        }
+    }
+
+    public void DoActivate()
+    {
+        Debug.Log(transform.name + " was activated!");
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+  
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}

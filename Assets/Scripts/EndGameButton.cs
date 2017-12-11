3ccbecd26ae0a79f83072db3e2fb4af4196fc302
JackStrider <rossi.william@gmail.com>
2017-12-10 23:04:54 -0600

    Made room, end game button, and most of the game

---
+++ Assets/Scripts/EndGameButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameButton : MonoBehaviour, IActivatable
+{     
+        [SerializeField]
+        private string nameText;
+
+        public string NameText { get { return nameText; } }
+
+        public void DoActivate()
+        {
+            // need to add using UnityEngine.UI;
+            
+        }    
+}
